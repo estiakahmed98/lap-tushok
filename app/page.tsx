b1763e9dd0bf9d3af22ac463af2be98365ebf08c
Estiak Ahmed <estiakahmed898@gmail.com>
2026-08-18 15:38:34 +0600

    Hero Updated

--- app/page.tsx
+++ app/page.tsx
@@ -47,10 +47,7 @@ export default function Home() {
             </div>
           </div>
           <div className="hero-media">
-            <img
-              src="Assets/hero shope.jpeg"
-              alt="পরিপাটি ও আরামদায়ক শোবার ঘর"
-            />
+            <img src="Assets/hero.jpeg" alt="পরিপাটি ও আরামদায়ক শোবার ঘর" />
           </div>
           <a href="#categories" className="hero-scroll">
             <ArrowDown /> নিচে দেখুন
